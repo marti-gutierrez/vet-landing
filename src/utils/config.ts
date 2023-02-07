9eb796bc943965d28634bfeeb798f01a222ebfb9
marti-gutierrez <martigh14@gmail.com>
2023-02-07 11:24:35 -0600

    feat[home]: add Bio component and config file

---
+++ src/utils/config.ts
@@ -0,0 +1,8 @@
+export const links = [{ to: "/", name: "home" }];
+
+export const bioInfo = {
+	name: "MVZ.Patricia Velasco Hdez",
+	lic: "UEAH LIC. Veterinaria y Zootecnia",
+	mision:
+		"La misión de la clínica veterinaria San Martín de Porres es prevenir y curar el dolor de nuestras amadas mascotas, brindándoles atención médica de alta calidad y mejorando sus vidas. Nos esforzamos por crear un ambiente cálido y acogedor, utilizando la medicina preventiva,curativa e higiene.Yo te curo, pero Dios te sana es nuestro lema que refleja nuestra confianza en la capacidad de sanación y el amorincondicional hacia las mascotas. ¡Bienvenido a San Martín de Porres!",
+};
